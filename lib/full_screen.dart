62bac497c57a87b57b00e90a181a42b864b8a954
Ali Taheri <taheri.aliTaheri.ali@gmail.com>
2024-12-11 11:00:01 +0330

    V 0.0.2 1403/09/21

--- lib/full_screen.dart
+++ lib/full_screen.dart
@@ -1,38 +1,49 @@
 import 'package:flutter/material.dart';
 
-class ImageSliderFullScreen extends StatefulWidget {
+class ImageFullScreen extends StatefulWidget {
   final String tagName;
   final ImageProvider imageProvider;
-  const ImageSliderFullScreen({super.key,
+  final Function()disposeWidget;
+  const ImageFullScreen({super.key,
     required this.tagName,
-    required this.imageProvider});
+    required this.imageProvider,
+    required this.disposeWidget,
+  });
 
   @override
-  State<ImageSliderFullScreen> createState() => _ImageSliderFullScreenState();
+  State<ImageFullScreen> createState() => _ImageFullScreenState();
 }
 
-class _ImageSliderFullScreenState extends State<ImageSliderFullScreen> {
+class _ImageFullScreenState extends State<ImageFullScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black12.withOpacity(.3),
-        iconTheme: Theme.of(context).iconTheme.copyWith(
-          color: Colors.white
+    return PopScope(
+      canPop: true,
+      onPopInvokedWithResult: (didPop, result) {
+        close();
+      },
+
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black12.withOpacity(.3),
+          iconTheme: Theme.of(context).iconTheme.copyWith(
+              color: Colors.white
+          ),
         ),
-      ),
-      backgroundColor: Colors.black12.withOpacity(.3),
-      body: InkWell(
-        onTap: () {
-          Navigator.pop(context);
-        },
-        child: Hero(
-          tag: widget.tagName,
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(45),
-                image: DecorationImage(image: widget.imageProvider,fit: BoxFit.scaleDown)
+        backgroundColor: Colors.black12.withOpacity(.3),
+        body: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+            close();
+          },
+          child: Hero(
+            tag: widget.tagName,
+            child: Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(45),
+                  image: DecorationImage(image: widget.imageProvider,fit: BoxFit.scaleDown)
+              ),
             ),
           ),
         ),
@@ -41,4 +52,7 @@ class _ImageSliderFullScreenState extends State<ImageSliderFullScreen> {
   }
 
 
+  void close(){
+    widget.disposeWidget();
+  }
 }
--- lib/widget.dart
+++ lib/widget.dart
@@ -1,10 +1,10 @@
+import 'dart:async';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'full_screen.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
-
-
-///اسلایدر image
+/// اسلایدر image
 class ImageSliderWidget extends StatefulWidget {
   ///لیست آدرس عکس ها
   final List<String> imagesList;
@@ -24,10 +24,59 @@ class ImageSliderWidget extends StatefulWidget {
 }
 
 class _ImageSliderWidgetState extends State<ImageSliderWidget> {
-
   int _currentIndex = 0;
   final PageController _pageController = PageController();
-  List<Color> colorBgList=[];
+  Timer? _sliderTimer;
+  bool _isPaused = false;
+
+
+  @override
+  void setState(VoidCallback fn) {
+    if (context.mounted) {
+      super.setState(fn);
+    }
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    _startSliderTimer();
+  }
+
+  void _startSliderTimer() {
+    _sliderTimer?.cancel(); // اگر تایمر قبلاً فعال بوده، آن را لغو می‌کنیم
+    _sliderTimer = Timer.periodic(const Duration(seconds: 3), (timer) {
+      if (!_isPaused) {
+        if (_currentIndex < widget.imagesList.length - 1) {
+          _currentIndex++;
+        } else {
+          _currentIndex = 0;
+        }
+        _pageController.animateToPage(
+          _currentIndex,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.fastOutSlowIn,
+        );
+      }
+    });
+  }
+
+  void _pauseSlider() {
+    setState(() {
+      _isPaused = true;
+    });
+    _sliderTimer?.cancel();
+  }
+
+  void _resumeSlider({int? secondDuration}) {
+    Future.delayed(Duration(seconds: secondDuration??0),() {
+      setState(() {
+        _isPaused = false;
+      });
+    },);
+    _startSliderTimer();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,6 +89,9 @@ class _ImageSliderWidgetState extends State<ImageSliderWidget> {
             setState(() {
               _currentIndex = index;
             });
+            _pauseSlider();
+            _resumeSlider(secondDuration: 0);
+
           },
           itemBuilder: (context, index) {
             return Center(
@@ -48,19 +100,36 @@ class _ImageSliderWidgetState extends State<ImageSliderWidget> {
                 cacheKey: widget.imagesList[index],
                 imageBuilder: (context, imageProvider) {
                   return InkWell(
+                    onTapDown: (_) => _pauseSlider(), // وقتی کاربر روی عکس کلیک می‌کند
+                    // onTapUp: (_) => _resumeSlider(), // وقتی کاربر دستش را برمی‌دارد
+                    onTapCancel: () => _resumeSlider(),
+                    onHover: (isFocus) {
+                      if(isFocus){
+                        _pauseSlider();
+                      }
+                      else{
+                        _resumeSlider();
+                      }
+                    }, // در هنگام Hover
                     onTap: () {
-                      Route route=MaterialPageRoute(builder: (context) => ImageSliderFullScreen(
-                        tagName: widget.imagesList[index],
-                        imageProvider: imageProvider,
-                      ),);
+                      Route route = MaterialPageRoute(
+                        builder: (context) => ImageFullScreen(
+                          tagName: widget.imagesList[index],
+                          imageProvider: imageProvider,
+                          disposeWidget: () => _resumeSlider(),
+                        ),
+                      );
                       Navigator.push(context, route);
                     },
                     child: Hero(
                       tag: widget.imagesList[index],
                       child: Container(
                         decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(45),
-                            image: DecorationImage(image: imageProvider,fit: BoxFit.scaleDown)
+                          borderRadius: BorderRadius.circular(45),
+                          image: DecorationImage(
+                            image: imageProvider,
+                            fit: BoxFit.scaleDown,
+                          ),
                         ),
                       ),
                     ),
@@ -68,31 +137,33 @@ class _ImageSliderWidgetState extends State<ImageSliderWidget> {
                 },
                 // fit: BoxFit.cover,
                 placeholder: (context, url) =>
-                    widget.placeholder?.call(context,url)??
+                widget.placeholder?.call(context,url)??
                     Center(
-                  child: CircularProgressIndicator(
-                    color: Theme.of(context).primaryColor,
-                    backgroundColor: Colors.transparent,
-                  ),
-                ),
+                      child: CircularProgressIndicator(
+                        color: Theme.of(context).primaryColor,
+                        backgroundColor: Colors.transparent,
+                      ),
+                    ),
                 // errorWidget: (context, url, error) => Icon(Icons.error),
                 errorWidget: (context, url, error) =>
-                    widget.errorWidget?.call(context,url,error)??
+                widget.errorWidget?.call(context,url,error)??
                     Icon(Icons.error,color: Theme.of(context).colorScheme.error,),
               ),
             );
           },
         ),
         Positioned(
-          bottom: 0,left: 0,right: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
           child: Align(
             alignment: Alignment.center,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: DecoratedBox(
                 decoration: BoxDecoration(
-                    color: Colors.grey.withOpacity(.3),
-                    borderRadius: BorderRadius.circular(10)
+                  color: Colors.grey.withOpacity(.3),
+                  borderRadius: BorderRadius.circular(10),
                 ),
                 child: Padding(
                   padding: const EdgeInsets.all(5.0),
@@ -101,20 +172,22 @@ class _ImageSliderWidgetState extends State<ImageSliderWidget> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: widget.imagesList.asMap().entries.map((entry) {
                       return GestureDetector(
-                        onTap: () => _pageController.animateToPage(entry.key,
-                            duration: Duration(milliseconds: 300), curve: Curves.easeIn),
+                        onTap: () => _pageController.animateToPage(
+                          entry.key,
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeIn,
+                        ),
                         child: Container(
                           width: 12.0,
                           height: 12.0,
-                          margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                          margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
                           decoration: BoxDecoration(
                             shape: BoxShape.circle,
-                            color: _currentIndex == entry.key?
-                            Theme.of(context).primaryColor:
-                            (Theme.of(context).brightness == Brightness.dark
+                            color: _currentIndex == entry.key
+                                ? Theme.of(context).primaryColor
+                                : (Theme.of(context).brightness == Brightness.dark
                                 ? Colors.white
-                                : Colors.black)
-                                .withOpacity(0.4),
+                                : Colors.black).withOpacity(0.4),
                           ),
                         ),
                       );
@@ -124,15 +197,15 @@ class _ImageSliderWidgetState extends State<ImageSliderWidget> {
               ),
             ),
           ),
-        )
-
+        ),
       ],
     );
   }
 
-
   @override
   void dispose() {
+
+    _sliderTimer?.cancel(); // تایمر را هنگام از بین رفتن ویجت لغو میکند
     _pageController.dispose();
     super.dispose();
   }
